c4d737ee1e60c6d56d0a556a1365163e0f3d60bb
Lukas Hlobil <lukas.hlobil@orbiszlin.cz>
2026-03-04 10:33:20 +0100

    Oprava import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
