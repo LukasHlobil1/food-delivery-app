11c568146e9a9907781081379e1d22a61856b18d
Lukas Hlobil <lukas.hlobil@orbiszlin.cz>
2026-03-04 09:54:00 +0100

    Routing | Hlavní stránky aplikace

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,22 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: 'restaurants',
+    loadComponent: () => import('./pages/restaurants/restaurants.page').then(m => m.RestaurantsPage)
+  },
+  {
+    path: 'restaurant/:id',
+    loadComponent: () => import('./pages/restaurant-detail/restaurant-detail.page').then(m => m.RestaurantDetailPage)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./pages/cart/cart.page').then(m => m.CartPage)
   },
 ];
